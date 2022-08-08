8b84220a479e98bfdbbe0065d2546f6b6a393d62
SvetlanaZt <svetlana.zt23@gmail.com>
2022-08-08 14:28:18 +0300

    hw

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,4 +1,5 @@
 import css from './FeedbackOptions.module.css'
+import PropTypes from 'prop-types';
 
 export default function FeedbackOptions({ options, onLeaveFeedback }) {
     return (   <div>
@@ -14,3 +15,8 @@ export default function FeedbackOptions({ options, onLeaveFeedback }) {
         ))}
         </div>)
 }
+
+FeedbackOptions.propTypes = {
+    options: PropTypes.arrayOf(PropTypes.string).isRequired,
+    onLeaveFeedback: PropTypes.func.isRequired,
+};
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,8 +1,10 @@
-import css from './Statistics.module.css'
+import css from './Statistics.module.css';
+import PropTypes from 'prop-types';
+import { nanoid } from 'nanoid'
 
 export default function Statistics({ good, neutral, bad, total, positivePercentage }) {
     return (
-      <ul className={css.statistics}>
+      <ul className={css.statistics} key={nanoid()}>
       <li className={css.item}>Good:{good}</li>
       <li className={css.item}>Neutral:{neutral}</li>
       <li className={css.item}>Bad:{bad}</li>
@@ -11,3 +13,12 @@ export default function Statistics({ good, neutral, bad, total, positivePercenta
         Positive feedback:{positivePercentage}%
       </li></ul>)
 }
+
+Statistics.propTypes = {
+    good: PropTypes.number.isRequired,
+    neutral: PropTypes.number.isRequired,
+    bad: PropTypes.number.isRequired,
+    total: PropTypes.number.isRequired,
+    positivePercentage: PropTypes.string.isRequired,
+
+};
